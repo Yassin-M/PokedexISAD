d6c80d258165554a74777cdc4c393342c12c14c2
Unaire123 <unair163@gmail.com>
2025-12-24 13:14:29 +0100

    He hecho el html+ he empezado con el backend pero es una movida

--- app/controller/view/bista_kontroladorea.py
+++ app/controller/view/bista_kontroladorea.py
@@ -1,4 +1,14 @@
+from flask import Blueprint, render_template, session
+from app.controller import eredu_kontroladorea
 class BistaKontroladorea:
    #metodos
    def __init__(self):
       pass
+
+   @bista_bp.route('/changelog')
+   def changelog():
+    ErabiltzaileIzena = "Ash"
+
+    lista_notificaciones = eredu_kontroladorea.notifikazioenInformazioaLortu(ErabiltzaileIzena)
+
+    return render_template('changelog.html', notificaciones=lista_notificaciones)
