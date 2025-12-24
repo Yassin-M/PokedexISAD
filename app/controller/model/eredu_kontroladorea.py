dbba2ce1d7555d90f566b1915c26448ba875326b
Unaire123 <unair163@gmail.com>
2025-12-24 13:15:22 +0100

    No se me habia guardado todo

--- app/controller/model/eredu_kontroladorea.py
+++ app/controller/model/eredu_kontroladorea.py
@@ -1,5 +1,10 @@
+from app.database import db
 class EreduKontroladorea:
 
    #metodos
    def __init__(self):
       pass
+
+   def notifikazioenInformazioaLortu():
+      query = "SELECT J.JarraituaIzena, N.DataOrdua, N.deskripzioa FROM JarraitzenDu J JOIN Notifikatu N ON J.JarraituaIzena = N.ErabiltzaileIzena WHERE J.JarraitzaileIzena = %s ORDER BY N.DataOrdua DESC;"
+      return db.select(query)
